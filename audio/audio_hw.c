b1acd5646f81703d307b24751ca6fcb3340ad33e
Christopher N. Hesse <raymanfx@gmail.com>
2016-02-02 16:39:51 +0100

    audio: Add support for SCO wideband calls
    Change-Id: Ice4fcb9d0017eee98c0ec909a111fc2409a87c91

--- audio/audio_hw.c
+++ audio/audio_hw.c
@@ -122,6 +122,14 @@ struct pcm_config pcm_config_sco = {
     .format = PCM_FORMAT_S16_LE,
 };
 
+struct pcm_config pcm_config_sco_wide = {
+    .channels = 1,
+    .rate = 16000,
+    .period_size = SCO_CAPTURE_PERIOD_SIZE,
+    .period_count = SCO_CAPTURE_PERIOD_COUNT,
+    .format = PCM_FORMAT_S16_LE,
+};
+
 struct pcm_config pcm_config_voice = {
     .channels = 2,
     .rate = 8000,
@@ -416,17 +424,25 @@ static void select_devices(struct audio_device *adev)
 /* must be called with hw device mutex locked, OK to hold other mutexes */
 static void start_bt_sco(struct audio_device *adev)
 {
+    struct pcm_config *sco_config;
+    
     if (adev->pcm_sco_rx != NULL || adev->pcm_sco_tx != NULL) {
         ALOGW("%s: SCO PCMs already open!\n", __func__);
         return;
     }
 
     ALOGV("%s: Opening SCO PCMs", __func__);
-
+    
+    if (adev->wb_amr) {
+        sco_config = &pcm_config_sco_wide;
+    } else {
+        sco_config = &pcm_config_sco;
+    }
+    
     adev->pcm_sco_rx = pcm_open(PCM_CARD,
                                 PCM_DEVICE_SCO,
                                 PCM_OUT | PCM_MONOTONIC,
-                                &pcm_config_sco);
+                                sco_config);
     if (adev->pcm_sco_rx != NULL && !pcm_is_ready(adev->pcm_sco_rx)) {
         ALOGE("%s: cannot open PCM SCO RX stream: %s",
               __func__, pcm_get_error(adev->pcm_sco_rx));
@@ -436,7 +452,7 @@ static void start_bt_sco(struct audio_device *adev)
     adev->pcm_sco_tx = pcm_open(PCM_CARD,
                                 PCM_DEVICE_SCO,
                                 PCM_IN,
-                                &pcm_config_sco);
+                                sco_config);
     if (adev->pcm_sco_tx && !pcm_is_ready(adev->pcm_sco_tx)) {
         ALOGE("%s: cannot open PCM SCO TX stream: %s",
               __func__, pcm_get_error(adev->pcm_sco_tx));
